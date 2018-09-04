ac02211e3bc4b72b8940bbe8292b140c199ee0be
anatollko <anatollko91@gmail.com>
2018-09-04 22:17:09 +0300

    HW1 - reverseString() - comments added

--- lesson-1/AnatoliyAvramenko/reverseString.js
+++ lesson-1/AnatoliyAvramenko/reverseString.js
@@ -1,13 +1,21 @@
 
 function reverseString(str) {
+
+    // splitting the argument to get an array of type [['w', 'o', 'r', 'd', 's'], [' ']]
     let arrayedStr = str.split(/([ ])/).map(el => {
         return el.split('');
     });
 
+    // iterating through each word of the argument unless it's a space or single letter
     arrayedStr.forEach((el, i) => {
         if (el.length < 2) return;
 
+        // declaring the var for object that would store non-letters and their indexes
+        // {index1: non-letter1, index2: non-letter2}
         let nonLetters = {};
+
+        // iterating through each char of each word of initial arg; 
+        // excluding non-letters and saving them to obj
         el.forEach((subEl, index) => {
             if (/[^A-Za-z]/.test(subEl)) {
                 nonLetters[index + Object.keys(nonLetters).length] = subEl;
@@ -15,19 +23,24 @@ function reverseString(str) {
             }
         });
 
+        // reversing the arrayed words
+        // if there are no non-letters, then joining chars back to words
         el.reverse();
         if (Object.keys(nonLetters).length === 0) {
             arrayedStr[i] = el.join('');
             return;
         }
 
+        // inserting non-letters back to their indexes into the words
         for (key in nonLetters) {
             el.splice(+key, 0, nonLetters[key]);
         }
 
+        // joining reversed chars with non-letters back to words
         arrayedStr[i] = el.join('');
     });
     
+    // joining words back to single string
     arrayedStr = arrayedStr.join('');
 
     return arrayedStr;
