e628a1728486d5d175ad43079e93a74413680d35
Dima <dmitriy@MacBook-Pro-1.local>
2018-09-05 23:17:51 +0300

    HW4 - add sort widget with lint debugging

--- lesson-4/Dnikolaienko/sort-widget/js/app.js
+++ lesson-4/Dnikolaienko/sort-widget/js/app.js
@@ -1,11 +1,37 @@
-let thead = document.querySelector('thead');
+const tabl = document.getElementById('tabl'); // получаем таблицу по айди
 
-thead.addEventListener('click', (e) => {
-    if(e.target.tagName !== 'TH') {
-        return;
-    }
+tabl.addEventListener('click', (e) => {
+  const targ = e.target; // записываем в константу нашу цель,которую по лучаем по клику в обработчике событий
+  if (targ.tagName !== 'TH') {
+    return; // если цель не является тегом ТН то выходим из функции
+  }
 
-    let tbody = e.target.tbody;
+  sortColumn(targ.cellIndex, targ.getAttribute('d-type')); // в другом случаем вызываем функцию сортировки
 
-    console.log(tbody);
 });
+
+function sortColumn(col, type) {
+  const tbody = tabl.getElementsByTagName('tbody')[0]; // получаем коллекцию ячееек тела таблицы
+  let sortCol; // инициализируем переменую которая будет в дальнейшем выполнять роль функции
+
+  const trArr = [].slice.call(tbody.rows); // превращаем коллекцию ячеек в массив
+
+  if (type === 'age') { // если тип переданого поля age сортируем по возврасту
+    sortCol = function(fRow, sRow) {
+      return fRow.cells[col].innerHTML - sRow.cells[col].innerHTML;
+    };
+  } else { // в ином случаем сортируем по имени
+    sortCol = function(fRow, sRow) {
+      return fRow.cells[col].innerHTML > sRow.cells[col].innerHTML;
+    };
+  }
+
+  trArr.sort(sortCol); // производим сортировку
+
+
+  for (let j = 0; j < trArr.length; j++) { // проходимся по ячейкам
+    tbody.appendChild(trArr[j]); // добавляем результат в тело таблицы
+  }
+
+  tabl.appendChild(tbody); // добавляем результат в таблицу
+}
