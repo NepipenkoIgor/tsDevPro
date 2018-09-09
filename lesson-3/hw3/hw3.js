f91bfd4c8a53354d097660f7f511e5502f35da12
Yuriy Shulga <avtomix555@gmail.com>
2018-09-09 23:26:40 +0300

    HW3 - Yuriy Shulga - after eslint check

--- lesson-3/hw3/hw3.js
+++ lesson-3/hw3/hw3.js
@@ -1,64 +1,74 @@
-(() => {
-    const next = node => {
-        let nextElem = node.nextElementSibling;
-        if(nextElem === null) {
-            return null; 
-        }
-        return nextElem.nodeType === 1 ? nextElem : next(nextElem);
+const next = (node) => {
+  let nextElem = node.nextElementSibling;
+  if (nextElem === null) {
+    return null;
+  }
+
+  return nextElem.nodeType === 1 ? nextElem : next(nextElem);
+};
+
+const addClass = (node, classToAdd) => {
+  if (node.classList.contains(classToAdd)) {
+    throw new Error(`Class ${classToAdd} is already exists`); // or just return node;
+  }
+
+  node.classList.add(classToAdd);
+  return node;
+};
+
+const removeClass = (node, classToRemove) => {
+  classToRemove.split(' ').forEach((toRemove) => {
+    if (node.classList.contains(toRemove)) {
+      node.classList.remove(toRemove);
+    }
+  });
+
+  return node;
+};
+
+const hasClass = (node, classToCheck) => node.classList.contains(classToCheck);
+
+const closest = (node, testFunc) => {
+  if (node.parentElement === null) {
+    return null;
+  }
+
+  if (!testFunc(node.parentElement)) {
+    return closest(node.parentElement, testFunc);
+  }
+
+  return node.parentElement;
+};
+
+const createList = (listData, listContainer = 'ul', itemContainer = 'li') => {
+  if (!Array.isArray(listData) || listData.length === 0) {
+    throw new Error(`Wrong arg: ${listData}`);
+  }
+
+  let li;
+  let list = document.createElement(listContainer);
+
+  listData.forEach((elem) => {
+    if (typeof elem !== 'string' && !Array.isArray(elem)) {
+      throw new Error(`Wrong type of arg: ${elem}`);
     }
 
-    const addClass = (node, classToAdd) => {
-        if(node.classList.contains(classToAdd)) {
-            throw new Error(`Class ${classToAdd} is already exists`); // or just return node;
-        }
-        return node.classList.add(classToAdd);
+    li = document.createElement(itemContainer);
+    if (typeof elem === 'string') {
+      li.textContent = elem;
+    } else {
+      li = createList(elem);
     }
 
-    const removeClass = (node, classToRemove) => {
-        classToRemove.split(' ').forEach(toRemove => {
-            if(node.classList.contains(toRemove)) {
-                node.classList.remove(toRemove);
-            }
-            return node; // for chaining
-        });
-        
-    } 
+    list.appendChild(li);
+  });
 
-    const hasClass = (node, classToCheck) => node.classList.contains(classToCheck);
+  return list;
+};
 
-    const closest = (node, testFunc) => {
-        if(node.parentElement === null) {
-            return null;
-        }
-
-        if(!testFunc(node.parentElement)) {
-            return closest(node.parentElement, testFunc);
-        }
-
-        return node.parentElement;
-    }
-
-    const createList = (listData, listContainer='ul', itemContainer='li') => {
-        if(!Array.isArray(listData) || listData.length === 0) {
-            throw new Error(`Wrong arg: ${listData}`);
-        }
-
-        let li, list = document.createElement(listContainer);
-
-        listData.forEach(elem => {
-            if(typeof elem !== 'string' && !Array.isArray(elem)) {
-                throw new Error(`Wrong type of arg: ${elem}`);
-            }
-
-            li = document.createElement(itemContainer);
-            if(typeof elem === 'string') {
-                li.textContent = elem;
-            } else {
-                li = createList(elem);
-            }
-            list.appendChild(li);
-        });
-
-        return list;
-    }
-})();
+const header = document.querySelector('header');
+const menu = next(header);
+console.log(menu); // <menu></menu>
+console.log(addClass(menu, 'header')); // <menu class="header"></menu>
+console.log(hasClass(menu, 'header')); // true
+console.log(removeClass(header, 'header')); // <header class="slide selected"></header>
