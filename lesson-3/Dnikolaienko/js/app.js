10c1e22e84f55953db68ff130b1063143f457e6e
Dima <dmitriy@MacBook-Pro-1.local>
2018-09-09 18:03:39 +0300

    HW3 - use elint and add comment

--- lesson-3/Dnikolaienko/js/app.js
+++ lesson-3/Dnikolaienko/js/app.js
@@ -1,15 +1,15 @@
 /* 1) Реализовать функцию next(node), которая вернет следующий узел, не учитывая текстовые узлы и узлы комментариев. */
 
-let findChild = (function(node) {
-    try {
-    let nextEl = document.querySelector(node).nextElementSibling;
-    if(nextEl != null) {
-        return nextEl.nodeType != 3 || nextEl.nodeType != 8 ? nextEl : findChild(nextEl);
+const findChild = (function(node) { // объявляем безымяную функцию
+  try {
+    let nextEl = document.querySelector(node).nextElementSibling; // получаем следующий узел после переданного
+    if(nextEl != null) { // проверяем если узел не пуст
+        return nextEl.nodeType != 3 || nextEl.nodeType != 8 ? nextEl : findChild(nextEl); // тернарный оператор проверяет если узел не текст и не комментарии то возвращает его в противном случаем рекурсивно запускаем функцию
     } else {
-        throw new Error('Node dont be a empty, or text-element');
+        throw new Error('Node dont be a empty, or text-element'); // создаем эксепшен
     }
     } catch(e) {
-        console.log(e);
+        console.log(e); // ловим эексепшен и выводим его
     }
 })('#main');
 
@@ -17,42 +17,40 @@ console.log(findChild);
 
 /* 2) Реализовать функцию addClass(node, classToAdd). Класс не должен добавляться, если у элемента уже есть такой. */
 
-let addClass = (node, classToAdd) => {
-    let currEl = document.querySelector(node);
+const addClass = (node, classToAdd) => {
+  const currEl = document.querySelector(node); // получаем нужный нам узел
 
-    if (currEl.classList.contains(classToAdd)) {
-        throw new Error(`Class with same name => ${classToAdd} is already exists`);
-    }
+  if (currEl.classList.contains(classToAdd)) { // проверяем содержит ли наш узел заданный класс
+    throw new Error(`Class with same name => ${classToAdd} is already exists`); // если содержит то выкидываем ошибку
+  }
 
-    currEl.classList.remove(currEl.classList[0]);
-
-    return currEl.classList.add(classToAdd);
-}
+  return currEl.classList.toggle(classToAdd); // в другом случае используем функциию которая удаляет а потом добавляет класс
+};
 
 console.log(addClass('#main', 'myClass'));
 
 /* 3) Реализовать функцию removeClass(node, classToRemove). Удаление несуществующего класса не должно приводить к ошибке. Если классов несколько, должны быть удалены все. */
 
-let removeClass = (node, ...classToRemove) => {
-    let currEl = document.querySelector(node);
+const removeClass = (node, ...classToRemove) => {
+  const currEl = document.querySelector(node); // получаем нужный узел
 
-    for (cclass of classToRemove) {
-        if (currEl.classList.contains(cclass)) {
-            currEl.classList.remove(cclass);
-        } else {
-            throw new Error(`Class with same name => ${cclass} is doesn't exists`);
-        }
+  for (let c = 0; c < classToRemove.length; c++) {
+    if (currEl.classList.contains(classToRemove[c])) { // проверяем содержит ли текущий класс,класс который в поиске
+      currEl.classList.remove(classToRemove[c]); // удаляем если есть подходящий класс
+    } else {
+      throw new Error(`Class with same name => ${classToRemove[c]} is doesn't exists`); // вызываем ошибку
     }
-}
+  }
+};
 
 console.log(removeClass('#main', 'test', 'spice'));
 
 /* 4) Реализовать функцию hasClass(node, classToCheck), которая вернет true, если у node есть класс classToCheck */
 
 function hasClass(node, findClass) {
-    let el = document.querySelector(node);
+  const el = document.querySelector(node);
 
-    return el.classList.contains(findClass);
+  return el.classList.contains(findClass);
 }
 
 console.log(hasClass('#main', 'test'));
@@ -62,23 +60,29 @@ console.log(hasClass('span', 'spice'));
     В testFunc получает аргументом DOM узел. Сам DOM узел node тоже проверять. Если ни один из родителей не подошел, функция возвращает null
 */
 
-let testFunc = (node) => {
-    let el = document.querySelector(node);
-    if (el) {
-        return true;
-    } else {
-        return false;
-    }
-}
+const testFunc = (node) => {
+  const el = document.querySelector(node);
+  let answer;
+  if (el) {
+    answer = true; // если нужный узел найдет возв. тру
+  } else {
+    answer = false; // если нужный узел не найден возв. фолс
+  }
+
+  return answer;
+};
 
 function closest(node) {
-    let el = document.querySelector(node);
+  const el = document.querySelector(node); // получаем нужный узел
+  let answer;
 
-    if (testFunc(node) == true && el != false) {
-        return el.parentElement;
-    } else {
-        return null;
-    }
+  if (testFunc(node) === true && el !== false) { // если ответ функции тестФанк вернул тру и узен не пуст
+    answer = el.parentElement; // то получаем его первого родителя
+  } else {
+    answer = null; // в ином случаем получаем нулл
+  }
+
+  return answer;
 }
 
 console.log(closest('body'));
@@ -92,25 +96,25 @@ console.log(closest('body'));
 */
 
 function createList(listData, listContainer = 'ul', itemContainer = 'li') {
-    if (!Array.isArray(listData) || typeof listData === 'string') {
-        throw new Error('Data must be array or string');
-    }
+  if (!Array.isArray(listData) || typeof listData !== 'string') { // проверяем первый аргумент не массив или не строка
+    throw new Error('Data must be array or string'); // вызываем ошибку
+  }
 
-    let ul = document.createElement(listContainer);
+  const ul = document.createElement(listContainer); // создаем ul элемент
 
-    for (element of listData) {
-        let li = document.createElement(itemContainer);
-        if(Array.isArray(element)) {
-            let newLi = createList(element);
-            li.appendChild(newLi);
-        } else {
-            li.textContent = element;
-        }
-        ul.appendChild(li);
+  for (let a = 0; a < listData.length; a++) { // проходим по данным
+    const li = document.createElement(itemContainer); // создаем li элемент
+    if (Array.isArray(listData[a])) { // если данные массв
+      const newLi = createList(listData[a]); // создаем тогда перечень элементов
+      li.appendChild(newLi); // присваем их li элементу
+    } else {
+      li.textContent = listData[a]; // в ином случае нам пришли строчные элементы
     }
-    return ul;
+    ul.appendChild(li); // добавляем все в ul
+  }
+  return ul;
 }
 
-let arr = ['mini','test','elements','next','node','end'];
+const arr = ['mini', 'test', 'elements', 'next', 'node', 'end'];
 
 console.log(createList(arr));
