f8368fab7db29c811bc31f79e135bb150d56a017
Yuriy Shulga <avtomix555@gmail.com>
2018-09-14 09:09:56 +0300

    hw5 task 1 - Yuriy Shulga - CounterTS  (console version 2)

--- lesson-8/yuriyShulga/counter/counter.ts
+++ lesson-8/yuriyShulga/counter/counter.ts
@@ -1,47 +1,58 @@
 
-class Counter {
-  value: number;
-  name: string;
-  private _running: any;
-  constructor(name = 'NoName') {
+interface ICounter {
+  getValue(): number;
+  refresh(): Counter; // those methods return counter for enable chaining ability
+  run(): Counter;
+  stop(): Counter;
+}
+
+class Counter implements ICounter {
+  public name: string;
+  private value: number;
+  private timeoutId: any;
+
+  constructor(name = "NoName") {
     this.value = 0;
     this.name = name;
- }
+  }
 
-  getValue() {
+  public getValue() {
     return this.value;
- }
+  }
 
-   inc() {
-    this.value += 1;
-    console.log(`${this.name} => ${this.value}`);
-    return this;
- }
-
-   dec() {
-    this.value -= 1;
-    return this;
- }
-
-   refresh() {
+  public refresh() {
     this.value = 0;
     return this;
- }
+  }
 
-   run() {
-    this._running = setInterval(this.inc.bind(this), 1000);
-    return this;
- }
-
-   stop() {
-    clearInterval(this._running);
+  public run() {
+    this.timeoutId = setInterval(this.inc.bind(this), 1000);
     return this;
   }
- };
 
+  public stop() {
+    console.log(`${this.name} stopped with value: ${this.value}`);
+    clearInterval(this.timeoutId);
+    delete this.timeoutId;
+    return this;
+  }
 
+  private inc() {
+    this.value += 1;
+    console.log(`${this.name}: ${this.value}`);
+    return this;
+  }
+}
 
-const counter1 = new Counter('First');
+const counter1 = new Counter("myCounter");
 console.log(counter1);
 counter1.run();
 setTimeout(counter1.stop.bind(counter1), 4000);
+setTimeout(() => console.log(counter1), 5000);
+
+const runBtn = document.querySelector(".run");
+const stopBtn = document.querySelector(".stop");
+const result = document.querySelector(".result");
+if (result instanceof Element) {
+  result.textContent = counter1.getValue().toString();
+}
